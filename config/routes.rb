c47753aa4ab54b0e34f81eabac63852de20a0669
socent <brad_wilcox@me.com>
2013-04-21 06:23:21 -0400

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RailsinstallerOsxDemo::Application.routes.draw do
-  get "welcome/index"
-
+  get 'about' => 'pages#about'
+  root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +50,7 @@ RailsinstallerOsxDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
